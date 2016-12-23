ab6b91629dd37c41c4b5157aaf035ebce77b1af7
Alexandr Shleyko <dusty@yandex-team.ru>
2016-12-23 10:10:24 +0300

    initial commit

---
+++ mail.php
@@ -0,0 +1,64 @@
+<?php
+mb_internal_encoding('utf-8');
+function fetch_email($data)
+{
+  var_dump($data);
+  foreach($data as $d) {
+    if (mb_strpos($d->text,'@')) return $d->text;
+  }
+}
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+$settings = [
+    'email' => 'dustyoo@yandex.ru',
+    'server' => 'imap.yandex.ru',
+    'port' => 993,
+    'protocol' => 'imap',
+    'username' => 'dustyoO',
+    'password' => '_Fktrcfylh1983!'
+];
+
+$emails_from = ['arshinsv@gmail.com'];
+
+$conn = "{{$settings['server']}:{$settings['port']}/{$settings['protocol']}/ssl}INBOX";
+
+echo $conn;
+
+$connection = imap_open(
+  $conn,
+  $settings['username'],
+  $settings['password']);
+if ($connection)
+{
+  echo 'ok';
+}
+
+$inbox = imap_check($connection);
+
+$msg_count = $inbox->Nmsgs;
+
+$start = $msg_count - 50;
+
+
+$result = imap_fetch_overview($connection,"{$start}:{$msg_count}",0);
+foreach ($result as $overview) {
+
+  $header = imap_headerinfo($connection, $overview->msgno);
+  $fromaddr = $header->from[0]->mailbox . "@" . $header->from[0]->host;
+
+
+  if (in_array($fromaddr, $emails_from))
+  {
+    $content = imap_body($connection, $overview->msgno);
+    preg_match_all("/<td\\sstyle=3D\"color:#2a2a2a.+?<a\\shref=3D\"([^\"]+?)\".+?<u><\/u>([^<]+?)<u><\/u><\/a>/ims", $content, $matches);
+    foreach ($matches[1] as $key => $value) {
+      echo "<a href=\"{$matches[1][$key]}\">{$matches[2][$key]}</a><br/>";
+    }
+    echo "<br/>";
+    echo "____________________________________________";
+    echo "<br/>";
+  }
+
+
+}
